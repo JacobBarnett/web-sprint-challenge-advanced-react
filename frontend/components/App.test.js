0733afe2a272fe670ce196048731be204bde84c6
JacobBarnett <93158517+JacobBarnett@users.noreply.github.com>
2022-02-11 13:39:38 -0800

    Finished project

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,28 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import AppFunctional from "./AppFunctional";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+
+test("b is on screen", () => {
+  render(<AppFunctional />);
+  const square = screen.getByTestId("active-square");
+  expect(square).toHaveTextContent("B");
+});
+
+test("move count, counts", () => {
+  render(<AppFunctional />);
+  const moved = screen.getByTestId("moved");
+  expect(moved).toHaveTextContent("You moved 0 times");
+  const left = screen.getByTestId("move-left");
+  left.click();
+  expect(moved).toHaveTextContent("You moved 1 times");
+  const up = screen.getByTestId("move-up");
+  up.click();
+  expect(moved).toHaveTextContent("You moved 2 times");
+});
+
+test("reset number to 0", () => {
+  render(<AppFunctional />);
+  const reset = screen.getByTestId("reset-number");
+  expect(reset).toHaveTextContent("reset");
+});
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,37 +1,105 @@
-import React from 'react'
+import { handleRequest } from "msw";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function AppFunctional(props) {
+  const [count, setCount] = useState(0);
+  const [{ x, y }, setCords] = useState({ x: 2, y: 2 });
+  const [email, setEmail] = useState("");
+  const [msg, setMsg] = useState("");
+  function move(x, y) {
+    setCount(count + 1);
+    setCords({ x: x, y: y });
+  }
+  const grid = [];
+  for (let yy = 1; yy <= 3; yy++) {
+    for (let xx = 1; xx <= 3; xx++) {
+      if (x === xx && y === yy) {
+        grid.push(
+          <div
+            data-testid="active-square"
+            key={`${xx}-${yy}`}
+            className="square active"
+          >
+            B
+          </div>
+        );
+      } else {
+        grid.push(<div key={`${xx}-${yy}`} className="square" />);
+      }
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .post("http://localhost:9000/api/result", {
+        x,
+        y,
+        email,
+        steps: count,
+      })
+      .then((response) => {
+        setMsg(response.data.message);
+      });
+  }
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">
+          Coordinates ({x}, {y})
+        </h3>
+        <h3 data-testid="moved" id="steps">
+          You moved {count} times
+        </h3>
       </div>
-      <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-      </div>
+      <div id="grid">{grid}</div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{msg}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button
+          data-testid="move-left"
+          id="left"
+          onClick={() => move(Math.max(1, x - 1), y)}
+        >
+          LEFT
+        </button>
+        <button
+          data-testid="move-up"
+          id="up"
+          onClick={() => move(x, Math.max(1, y - 1))}
+        >
+          UP
+        </button>
+        <button id="right" onClick={() => move(Math.min(3, x + 1), y)}>
+          RIGHT
+        </button>
+        <button id="down" onClick={() => move(x, Math.min(3, y + 1))}>
+          DOWN
+        </button>
+        <button
+          data-testid="reset-number"
+          id="reset"
+          onClick={() => {
+            setCords({ x: 2, y: 2 });
+            setCount(0);
+          }}
+        >
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          id="email"
+          type="email"
+          placeholder="type email"
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
